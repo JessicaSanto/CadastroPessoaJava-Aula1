caa29f0845cf1bfbd3fd89077e66b7ad9182e3cc
JessicaSanto <jessicaesanto@outlook.com>
2023-10-05 15:10:08 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,48 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Bem vindo ao sistema de cadastro de Pessoas Físicas e Juridicas");
 
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+        do {
+
+            System.out.println("Escolha uma opção: 1 - Pessoa Física / 2 - Pessoa Jurídica / 0 - Sair");
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    int opcaoPf;
+
+                    do {
+                        System.out.println("Escolha uma opção: 1 - Cadastrar Pessoa Física / 2 - Listar Pessoa Física / 0 - Voltar ao menu anterior");
+
+                        opcaoPf = scanner.nextInt();
+
+                        switch (opcaoPf) {
+                            case 1:
+
+                                break;
+                            case 2:
+                                break;
+                            case 0:
+                                break;
+                            default:
+                                break;
+                        }
+
+                    } while (opcaoPf != 0);
+                    break;
+
+                case 2:
+                    break;
+
+                case 0:
+                    break;
+            }
+        } while (opcao != 0);
 
     }
 }
